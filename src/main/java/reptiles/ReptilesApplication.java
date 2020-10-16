36266111d61cd64b61200abc6ed5f8dbd207f731
わらい <shenjinsong@youxiuqn.com>
2020-10-16 15:41:00 +0800

    gx

--- src/main/java/reptiles/ReptilesApplication.java
+++ src/main/java/reptiles/ReptilesApplication.java
@@ -2,17 +2,15 @@ package reptiles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"reptiles"})
+@ComponentScan(basePackages = {"com", "reptiles"})
 public class ReptilesApplication {
 
     public static void main(String[] args) {
